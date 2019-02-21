f6b4c0361223b2646d29ab6dc1c2eb89f46f8ed8
Liam Cooke <liamjc22@gmail.com>
2019-02-21 18:07:48 -0500

    Added Enemies
    Added 3 types of enemies and scripts for them

---
+++ Space SHMUP Prototype/Assets/__Scripts/Enemy_1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1 : MonoBehaviour
+{
+    public float speed=.2f;
+    int direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Random.Range(0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction == 0)
+        {
+            Vector3 vec = new Vector3(0.5f, -0.5f, 0);
+            transform.position += vec * speed;
+        }
+        else if (direction == 1)
+        {
+            Vector3 vec = new Vector3(-0.5f, -0.5f, 0);
+            transform.position += vec * speed;
+        }
+    }
+}
---
+++ Space SHMUP Prototype/Assets/__Scripts/Enemy_2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_2 : MonoBehaviour
+{
+    float speed=.2f;
+    int direction;
+    float timeCount=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Random.Range(0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCount += Time.deltaTime;
+        if (direction == 0)
+        {
+            float x=(float)Mathf.Sin(timeCount);
+            float y=-.5f;
+            float z=0;
+            Vector3 vec = new Vector3(x, y, z);
+            transform.position += vec * speed;
+        }
+        else if (direction == 1)
+        {
+            float x = (float)Mathf.Sin(timeCount);
+            float y = -.5f;
+            float z = 0;
+            Vector3 vec = new Vector3(-x,y,z);
+            transform.position += vec * speed;
+        }
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+    }
+}
